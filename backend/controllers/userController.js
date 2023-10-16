293016a4a618a7018c8b350327e3ec3cf1675fda
Zifan Pan <brianpanzifan@gmail.com>
2023-10-16 16:08:34 -0400

    add customized error handler using asyncHandler pkg

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,10 @@
+import asyncHandler from "express-async-handler"; // allows use async await
+
 // @description  Authorize user/set token
 // @route        POST /api/users/authorize
 // @access       Public
-const authorizeUser = async (req, res) => {
+const authorizeUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Authorize User" });
-};
+});
 
 export { authorizeUser };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express"; // "type": "module" allows import syntax
 import dotenv from "dotenv";
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 
@@ -11,5 +12,8 @@ app.use("/api/users", userRoutes);
 
 // Routes
 app.get("/", (req, res) => res.send("Server is Ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
