35b93e91aab3f73badf424e5ed31fc3367650124
Zifan Pan <brianpanzifan@gmail.com>
2023-10-17 01:19:41 -0400

    add route protection (private) middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express"; // "type": "module" allows import syntax
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import connectDB from "./config/database.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json()); // allows parse raw json
 app.use(express.urlencoded({ extended: true })); // allows parse form data
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
