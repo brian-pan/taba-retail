984e80c3a163f0be6b2e41ad166e2e7b1c5851de
Zifan Pan <brianpanzifan@gmail.com>
2023-10-17 02:50:45 -0400

    add get user info and update user info func

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,11 +7,15 @@ import {
   getUserProfile,
   updateUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 // Connect routes with functions/controllers
 router.post("/", registerUser); // /api/users integrated
 router.post("/authorize", authorizeUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
