1251060039dc7267ba750ef3a647dd57b2874162
Zifan Pan <brianpanzifan@gmail.com>
2023-10-17 02:51:17 -0400

    code cleanup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -99,8 +99,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("User not found");
   }
-
-  res.status(200).json({ message: "Update User Profile" });
 });
 
 export {
