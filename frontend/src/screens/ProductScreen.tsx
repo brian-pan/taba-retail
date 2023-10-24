8b5e41531e8f367131ff8c1b242f6b51b497fc0b
Zifan Pan <brianpanzifan@gmail.com>
2023-10-23 20:20:41 -0400

    construct product details page

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 import products from "../assets/products";
@@ -9,9 +10,26 @@ const ProductScreen: React.FunctionComponent<ProductScreenProps> = () => {
   // const productId = useParams().id;
   const { id: productId } = useParams();
   const product = products.find((prod) => prod._id === productId);
-  console.log(product);
 
-  return <div>Product Screen</div>;
+  return (
+    <>
+      <div className="product-screen-wrapper">
+        <div className="product-screen__backwards-cta-wrapper">
+          <Link
+            className="button button-light button-borderless product-screen__backwards-cta"
+            to="/"
+          >
+            Back
+          </Link>
+        </div>
+        <div className="product-screen__body-wrapper">
+          <div className="product-screen__image">
+            <img src={product?.image} alt={product?.name} aria-hidden={true} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductScreen;
