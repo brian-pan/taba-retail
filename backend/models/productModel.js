18ec16a03f2628ecf282cf15a2691aa0d2b071d7
Zifan Pan <brianpanzifan@gmail.com>
2023-10-24 15:07:47 -0400

    add BE-DB Product Model

---
+++ backend/models/productModel.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+    isInStock: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    numberInStock: {
+      type: Number,
+      required: false,
+    },
+    numberReviews: {
+      type: Number,
+      required: false,
+    },
+    rating: {
+      type: Number,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -11,7 +11,7 @@ interface Product {
   category: string;
   isInStock?: boolean;
   image: string;
-  description: string;
+  description?: string;
   unit?: string;
   rating?: number;
   numberReviews?: number;
