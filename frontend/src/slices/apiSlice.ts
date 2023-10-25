d40d6c32000f7ab5dc6019a311d0f84b63a17097
Zifan Pan <brianpanzifan@gmail.com>
2023-10-25 14:05:18 -0400

    add app constants

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -1,13 +1,14 @@
 // make async call to BE with thunk middleware
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
 
 // boilerplate
 const baseQuery = fetchBaseQuery({
-  baseUrl: "",
+  baseUrl: BASE_URL,
 });
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ["User", "Products", "Orders"], //cache
+  tagTypes: ["User", "Product", "Order"], //cache
   endpoints: (builder) => ({}),
 });
