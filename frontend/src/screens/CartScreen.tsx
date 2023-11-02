7c13abc4de32d30c97ab3fe0c2328c0a7b3ad039
Zifan Pan <brianpanzifan@gmail.com>
2023-11-02 02:10:33 -0400

    add cart screen ui

--- frontend/src/screens/CartScreen.tsx
+++ frontend/src/screens/CartScreen.tsx
@@ -4,13 +4,83 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { FaTrash } from "react-icons/fa";
 import Message from "../components/Message";
+import { cartItemType } from "../types";
 
 interface CartScreenProps {}
 
 const CartScreen: React.FunctionComponent<CartScreenProps> = (props) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  return <div>cart screen</div>;
+
+  // Get global cart state
+  // @ts-ignore
+  const cartState = useSelector((state) => state.cart);
+  const { cartItems } = cartState;
+
+  // Define booleans
+  const isCartEmpty = cartItems.length === 0;
+
+  return (
+    <div className="cart-screen-wrapper">
+      <h1 className="cart-screen__heading">Shopping Cart</h1>
+      <div className="cart-screen__body-wrapper">
+        {isCartEmpty ? (
+          <>
+            <p className="cart-screen__text--is-empty">
+              Your cart is empty. <Link to="/">Go Back</Link>
+            </p>
+          </>
+        ) : (
+          <>
+            <div className="cart-screen__items">
+              {cartItems.map((cartItem: cartItemType) => (
+                <div className="cart-screen__item">
+                  <img
+                    className="cart-screen__item-image"
+                    src={cartItem.image}
+                    alt={cartItem.name}
+                  />
+
+                  <Link
+                    className="cart-screen__item-name"
+                    to={`/products/${cartItem._id}`}
+                  >
+                    {cartItem.name}
+                  </Link>
+
+                  <p className="cart-screen__item-price">${cartItem.price}</p>
+
+                  <div className="cart-screen__item-qty-wrapper">
+                    <div className="cart-screen__item-qty__label">
+                      Quantity:
+                    </div>
+                    <div className="cart-screen__item-qty__btns">
+                      <button
+                        className="cart-screen__item-qty__btn cart-screen__item-qty__btn--minus"
+                        disabled={false}
+                      >
+                        -
+                      </button>
+                      <span className="cart-screen__item-qty__btn-label">
+                        {cartItem.qty}
+                      </span>
+                      <button className="cart-screen__item-qty__btn cart-screen__item-qty__btn--plus">
+                        +
+                      </button>
+                    </div>
+                  </div>
+
+                  <button className="cart-screen__item-btn">
+                    <FaTrash />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CartScreen;
