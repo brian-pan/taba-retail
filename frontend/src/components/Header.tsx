204a81e96410e0b4c9a7b4e2aba32e830dd0d8a9
Zifan Pan <brianpanzifan@gmail.com>
2023-11-05 23:10:49 -0500

    fix header cart btn not render after sign in issue

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -58,79 +58,81 @@ const Header: React.FunctionComponent<HeaderProps> = (props) => {
         </Link>
       </div>
 
-      {userInfo ? (
-        <>
-          <div className="header__dropdown-component">
-            <div className="header__dropdown-wrapper">
-              <button
-                className={`header__dropdown-icon-wrapper ${
-                  isDropDownOpen && "header__dropdown-icon-wrapper--is-open"
-                }`}
-                onClick={() => setIsDropDownOpen(!isDropDownOpen)}
-              >
-                <IoCaretDown
-                  className={`header__dropdown-icon ${
-                    isDropDownOpen && "header__dropdown-icon--is-open"
-                  }`}
-                />
-              </button>
-              <div className="header__dropdown-name-wrapper">
-                {userInfo.name}
-              </div>
-            </div>
-            {isDropDownOpen && (
-              <div className="header__dropdown-content-wrapper">
-                <Link className="header__dropdown-content" to="/profile">
-                  <span className="header__dropdown-content-icon-wrapper">
-                    <FaUser className="header__dropdown-content-icon" />
-                  </span>
-                  <span
-                    className="header__dropdown-content-label"
-                    onClick={() => setIsDropDownOpen(false)}
-                  >
-                    Update Profile
-                  </span>
-                </Link>
-                <Link
-                  className="header__dropdown-content"
-                  to="/"
-                  onClick={handleLogout}
-                >
-                  <span className="header__dropdown-content-icon-wrapper">
-                    <FaSignOutAlt className="header__dropdown-content-icon" />
-                  </span>
-                  <span className="header__dropdown-content-label">
-                    Sign Out
-                  </span>
-                </Link>
-              </div>
-            )}
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="header__links">
-            <Link className="header__link header__link--sign-up" to="/cart">
-              <FaShoppingCart className="header__link-icon header__link-icon--cart" />
-              Cart
-              {cartItems.length > 0 && (
-                <span>
-                  {cartItems.reduce(
-                    (acc: number, curr: cartItemType) => acc + curr.qty,
-                    0
-                  )}
-                </span>
+      <div>
+        <Link className="header__link header__link--sign-up" to="/cart">
+          <FaShoppingCart className="header__link-icon header__link-icon--cart" />
+          Cart
+          {cartItems.length > 0 && (
+            <span>
+              {cartItems.reduce(
+                (acc: number, curr: cartItemType) => acc + curr.qty,
+                0
               )}
-            </Link>
-            <Link className="header__link header__link--sign-in" to="/login">
-              <span>
-                <FaSignInAlt className="header__link-icon header__link-icon--sign-in" />
-              </span>
-              <span>Sign In</span>
-            </Link>
-          </div>
-        </>
-      )}
+            </span>
+          )}
+        </Link>
+        {userInfo ? (
+          <>
+            <div className="header__dropdown-component">
+              <div className="header__dropdown-wrapper">
+                <button
+                  className={`header__dropdown-icon-wrapper ${
+                    isDropDownOpen && "header__dropdown-icon-wrapper--is-open"
+                  }`}
+                  onClick={() => setIsDropDownOpen(!isDropDownOpen)}
+                >
+                  <IoCaretDown
+                    className={`header__dropdown-icon ${
+                      isDropDownOpen && "header__dropdown-icon--is-open"
+                    }`}
+                  />
+                </button>
+                <div className="header__dropdown-name-wrapper">
+                  {userInfo.name}
+                </div>
+              </div>
+              {isDropDownOpen && (
+                <div className="header__dropdown-content-wrapper">
+                  <Link className="header__dropdown-content" to="/profile">
+                    <span className="header__dropdown-content-icon-wrapper">
+                      <FaUser className="header__dropdown-content-icon" />
+                    </span>
+                    <span
+                      className="header__dropdown-content-label"
+                      onClick={() => setIsDropDownOpen(false)}
+                    >
+                      Update Profile
+                    </span>
+                  </Link>
+                  <Link
+                    className="header__dropdown-content"
+                    to="/"
+                    onClick={handleLogout}
+                  >
+                    <span className="header__dropdown-content-icon-wrapper">
+                      <FaSignOutAlt className="header__dropdown-content-icon" />
+                    </span>
+                    <span className="header__dropdown-content-label">
+                      Sign Out
+                    </span>
+                  </Link>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="header__links">
+              <Link className="header__link header__link--sign-in" to="/login">
+                <span>
+                  <FaSignInAlt className="header__link-icon header__link-icon--sign-in" />
+                </span>
+                <span>Sign In</span>
+              </Link>
+            </div>
+          </>
+        )}
+      </div>
     </header>
   );
 };
