b8d382fcf66af4593c74c70a64c052d7ab1810f3
Zifan Pan <brianpanzifan@gmail.com>
2023-11-12 16:55:43 -0500

    add payment screen page

---
+++ frontend/src/screens/PaymentScreen.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface PaymentScreenProps {}
+
+const PaymentScreen: React.FunctionComponent<PaymentScreenProps> = (props) => {
+  return <div>Payment Screen</div>;
+};
+
+export default PaymentScreen;
