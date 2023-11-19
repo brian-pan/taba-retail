93fd791ad1573df6c035e7fb8f3abebbef82cd8e
Zifan Pan <brianpanzifan@gmail.com>
2023-11-19 15:24:29 -0500

    add payment screen back cta

--- frontend/src/screens/PaymentScreen.tsx
+++ frontend/src/screens/PaymentScreen.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 interface PaymentScreenProps {}
 
 const PaymentScreen: React.FunctionComponent<PaymentScreenProps> = (props) => {
-  return <div>Payment Screen</div>;
+  return (
+    <div className="payment-screen-wrapper">
+      <div className="payment-screen__back-cta-wrapper">
+        <Link className="payment-screen__back-cta" to="/checkout">
+          Back
+        </Link>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default PaymentScreen;
