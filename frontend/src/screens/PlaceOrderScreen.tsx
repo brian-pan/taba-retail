9b2de343ddb9f52a00a50c0cc2c80df3eec07c90
Zifan Pan <brianpanzifan@gmail.com>
2023-11-26 19:57:33 -0500

    add place order basic logic

--- frontend/src/screens/PlaceOrderScreen.tsx
+++ frontend/src/screens/PlaceOrderScreen.tsx
@@ -1,10 +1,26 @@
 import * as React from "react";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 interface PlaceOrderScreenProps {}
 
 const PlaceOrderScreen: React.FunctionComponent<PlaceOrderScreenProps> = (
   props
 ) => {
+  //@ts-ignore
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress, paymentMethod } = cart.cartOrderInfo;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate("/checkout");
+    } else if (!paymentMethod) {
+      navigate("/payment");
+    }
+  }, [shippingAddress, paymentMethod, navigate]);
+
   return <div>Place Order</div>;
 };
 
